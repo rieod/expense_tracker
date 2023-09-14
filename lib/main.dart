32e79a72cf7f57a3a1a52a2317f1afe971199efc
sariwahyu <124552096+rieod@users.noreply.github.com>
2023-09-15 01:50:36 +0700

    getting user input

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Expenses(),
+    theme: ThemeData(useMaterial3: true),
+    home: const Expenses(),
   ));
 }
